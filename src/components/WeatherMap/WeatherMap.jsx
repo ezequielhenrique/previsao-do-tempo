d6456cca76e3a61e6fb68ff2e351ff3637d4ce73
ezequielhenrique <ezequielhenrique806@gmail.com>
2025-05-09 16:44:58 -0300

    fix bug of marker

--- src/components/WeatherMap/WeatherMap.jsx
+++ src/components/WeatherMap/WeatherMap.jsx
@@ -2,6 +2,20 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import RecenterMap from '../RecentMap';
 import './WeatherMap.css';
 
+// Código para correção de bug do ícone
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
+
 function WeatherMap({ lat, lon, city }) {
   return (
     <MapContainer center={[lat, lon]} zoom={12} className='map-container'>
